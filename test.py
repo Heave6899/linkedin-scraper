d7bf204aa3876e3c2cd4fe6ed4c18a8a49433a6c
Vansh Patel <vnshptel@gmail.com>
2021-11-09 01:54:56 -0700

    Headless Argument Added

--- test.py
+++ test.py
@@ -54,7 +54,7 @@ def chrome_webdriver():
         PROXY_PASS = PROXY_CHEAP_PW # password
 
         options = Options()
-        # options.add_argument('--headless')
+        options.add_argument('--headless')
         options.add_argument("--window-size=1920x1080")
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-gpu')
