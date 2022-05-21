2a8f1fae6dae1bb16eace69b41401ed54b1f2064
Vansh Patel <vanshfpatel@outlook.com>
2022-05-20 18:51:34 -0700

    Update test.py

--- test.py
+++ test.py
@@ -18,7 +18,7 @@ import sys
 def connect_to_db():
     client = MongoClient(
         "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false")
-    db = client.cloudeagle
+    db = client.db
     return db
 
 
